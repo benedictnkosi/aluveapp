0be2e66a45dacc4621c0b8f4120608fc23a4ee20
benedictnkosi <nkosi.benedict@gmail.com>
2022-11-28 19:43:00 +0200

    cleaning icon on calendar

--- src/Service/ICalApi.php
+++ src/Service/ICalApi.php
@@ -237,7 +237,7 @@ class ICalApi
                         $status = $this->em->getRepository(ReservationStatus::class)->findOneBy(array('name' => 'confirmed'));
                         $reservation->setCheckIn(new DateTime($checkInDate));
                         $reservation->setCheckOut(new DateTime($checkOutDate));
-                        $reservation->setStatus(new DateTime($status));
+                        $reservation->setStatus($status);
                         $this->em->persist($reservation);
                         $this->em->flush($reservation);
 
