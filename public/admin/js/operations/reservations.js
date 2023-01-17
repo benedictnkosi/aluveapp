10798803f7adccbf4990e46077922d717c1bd298
benedictnkosi <nkosi.benedict@gmail.com>
2023-01-17 13:43:38 +0200

    cleaning icon on calendar

--- public/admin/js/operations/reservations.js
+++ public/admin/js/operations/reservations.js
@@ -644,6 +644,10 @@ function addPayment(event) {
 
         if (paymentChannel.localeCompare("cash") === 0) {
             paymentReference = "cash";
+            if (paymentReference.length > 1) {
+                showResErrorMessage("reservation", "Cash payments do not have a reference");
+                return;
+            }
         }
 
         isUserLoggedIn();
