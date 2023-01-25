efa0b361e663f190740dd87e04744ca5476399d3
benedictnkosi <nkosi.benedict@gmail.com>
2023-01-25 12:07:01 +0200

    cleaning icon on calendar

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
 
 class EmailService
 {
