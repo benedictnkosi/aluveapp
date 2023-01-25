0f398c24ed331657004d5ba61a71ee24dafcb18f
benedictnkosi <nkosi.benedict@gmail.com>
2023-01-25 10:16:46 +0200

    cleaning icon on calendar

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+
+    /**
+     * @Route("api/mail/send")
+     */
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $responseArray = array();
+
+        $email = (new Email())
+            ->from('info@aluvegh.co.za')
+            ->to('admin@aluvegh.co.za')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        try {
+
+            $mailer->send($email);
+            $responseArray[] = array(
+                'result_message' => "Success",
+                'result_code'=> 0
+            );
+        } catch (\Exception $e) {
+            $responseArray[] = array(
+                'result_message' =>"Fail",
+                'result_code'=> 1
+            );
+        }
+
+        return new JsonResponse($responseArray , 200, array());
+    }
+}
--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -2,7 +2,15 @@
 
 namespace App\Service;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
 class EmailService
 {
+
+
 
 }
